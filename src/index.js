7990ac15358a2a42725cbf6f2ee56e7a64692fa5
abbosjon-kudratov <abbosjon.kudratov@gmail.com>
2019-07-08 10:39:37 +0500

    done, every test works fine

--- src/index.js
+++ src/index.js
@@ -4,4 +4,31 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+
+   let triangles = 0;
+  const pairs = {};
+
+  preferences.forEach((x, i) => (pairs[i + 1] = x));
+  Object.keys(pairs).forEach(pair => !pairs[pair] && delete pairs[pair]);
+
+  for (const key in pairs) {
+    if (pairs.hasOwnProperty(key)) {
+      if (key == pairs[key]) {
+        delete pairs[key];
+      }
+
+      let start = key;
+      let end = pairs[pairs[pairs[key]]];
+
+      if (start == end) {
+        delete pairs[key];
+        delete pairs[pairs[key]];
+        delete pairs[pairs[pairs[key]]];
+
+        triangles++;
+      }
+    }
+  }
+
+  return triangles;
 };
